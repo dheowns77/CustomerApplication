cae2f73c03c1e56d3f4a9c213b0d73ba881f46af
Jo Eun Park <ipark10@myseneca.ca>
2020-04-16 18:24:53 -0400

    Switch btwn activities

--- app/src/main/java/com/example/cutomerapplication/MainActivity.java
+++ app/src/main/java/com/example/cutomerapplication/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.cutomerapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        configureSearchButton();
+
+
     }
 
-    public void onClickButton(View view){
+   /* public void onClickButton(View view){
         Toast toast = Toast.makeText(this,"Test",Toast.LENGTH_SHORT);
         toast.show();
 
     }
+    */
+
+
+   private void configureSearchButton(){
+
+       Button searchButton = (Button) findViewById(R.id.searchButton);
+       searchButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent (MainActivity.this, SearchActivity.class));
+           }
+       });
+
+   }
 }
---
+++ app/src/main/java/com/example/cutomerapplication/SearchActivity.java
@@ -0,0 +1,28 @@
+package com.example.cutomerapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        configureBackButton();
+    }
+
+    private void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
