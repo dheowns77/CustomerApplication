2b77a06e14f09836d0b6a46646800eaddd3c64f3
dheowns77 <dheowns77@gmail.com>
2020-04-22 01:49:59 -0400

    Category Update

--- app/src/main/java/com/example/cutomerapplication/CurOrderActivity.java
+++ app/src/main/java/com/example/cutomerapplication/CurOrderActivity.java
@@ -28,23 +28,24 @@ public class CurOrderActivity extends AppCompatActivity {
 
 
         Intent item = getIntent();
-        int value1 = item.getExtras().getInt("num1",-1);
-        if(value1 !=-1){
-            int value2 = item.getExtras().getInt("num2",-1);
-            int value3 = item.getExtras().getInt("num3",-1);
-            int value4 = item.getExtras().getInt("num4",-1);
-            int value5 = item.getExtras().getInt("num5",-1);
-            int total = item.getExtras().getInt("price",-1);
-            ((TextView)findViewById(R.id.cur_order_text_1)).setText("1. Glue(10EA) : "+value1 +" Order");
-            ((TextView)findViewById(R.id.cur_order_text_2)).setText("2. Glue(10EA) : "+value2+" Order");
-            ((TextView)findViewById(R.id.cur_order_text_3)).setText("3. Glue(10EA) : "+value3+" Order");
-            ((TextView)findViewById(R.id.cur_order_text_4)).setText("4. Glue(10EA) : "+value4+" Order");
-            ((TextView)findViewById(R.id.cur_order_text_5)).setText("5. Glue(10EA) : "+value5+" Order");
-            ((TextView)findViewById(R.id.cur_order_text_total)).setText("Total : $"+total+".00");
-            ((TextView)findViewById(R.id.cur_order_promote)).setText("Order $"+(100.00-(double)total)+" more today and get free delivery for next order!");
+        if(item.getExtras()!=null) {
+            int value1 = item.getExtras().getInt("num1", -1);
+            if (value1 != -1) {
+                int value2 = item.getExtras().getInt("num2", -1);
+                int value3 = item.getExtras().getInt("num3", -1);
+                int value4 = item.getExtras().getInt("num4", -1);
+                int value5 = item.getExtras().getInt("num5", -1);
+                int total = item.getExtras().getInt("price", -1);
+                ((TextView) findViewById(R.id.cur_order_text_1)).setText("1. Glue(10EA) : " + value1 + " Order");
+                ((TextView) findViewById(R.id.cur_order_text_2)).setText("2. Glue(10EA) : " + value2 + " Order");
+                ((TextView) findViewById(R.id.cur_order_text_3)).setText("3. Glue(10EA) : " + value3 + " Order");
+                ((TextView) findViewById(R.id.cur_order_text_4)).setText("4. Glue(10EA) : " + value4 + " Order");
+                ((TextView) findViewById(R.id.cur_order_text_5)).setText("5. Glue(10EA) : " + value5 + " Order");
+                ((TextView) findViewById(R.id.cur_order_text_total)).setText("Total : $" + total + ".00");
+                ((TextView) findViewById(R.id.cur_order_promote)).setText("Order $" + (100.00 - (double) total) + " more today and get free delivery for next order!");
 
+            }
         }
-
     }
 
     public void goBackToMain(View view){
